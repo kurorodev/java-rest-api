2b192fb52bebb180400f7efe6ee9a4e10d2a7bb5
Daniil Kudryavcev <83886080+kurorodev@users.noreply.github.com>
2022-09-09 22:44:46 +0400

    error 9008

--- src/main/java/com/example/restapiservice/controller/User_Controller.java
+++ src/main/java/com/example/restapiservice/controller/User_Controller.java
@@ -1,13 +1,35 @@
 package com.example.restapiservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.restapiservice.repository.UserRepo;
+
+import entity.UserEntity;
+
 @RestController
-@RequestMapping("/users")
+@RequestMapping
 public class User_Controller {
+    @Autowired
+    private UserRepo userRepo;
+
+    @PostMapping("/users/")
+    public ResponseEntity registration(@RequestBody UserEntity user) {
+        try {
+            userRepo.save(user);
+            return ResponseEntity.ok("Пользователь был успешно сохранен");
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+
+
+
     @GetMapping
     public ResponseEntity getUsers(){
         try {
---
+++ src/main/java/com/example/restapiservice/repository/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.restapiservice.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import entity.UserEntity;
+
+public interface UserRepo extends CrudRepository<UserEntity, Long>{
+}
